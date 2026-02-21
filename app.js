c3e6bbffa3a3c5a55d4d9c5707ef402ddcadb450
msoyabcode <mohdsoyab0785@gmail.com>
2026-02-21 23:48:02 +0530

    Initial CRUD setup with create and read user

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const userModel = require('./models/user')
+
+app.set("view engine", "ejs")
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/read", (req, res) => {
+    res.render("read")
+})
+
+app.post("/create", async (req, res) => {
+    let {name, email, image} = req.body;
+
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    })
+    res.send(createdUser)
+})
+app.listen(3000)
